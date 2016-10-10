4f127e6206ac1e89fe2a947766bee134fdf099fb
songs <songsofgrazier@hotmail.com>
2016-10-10 21:38:05 +0800

    class

---
+++ Classes/RandomMap.cpp
@@ -0,0 +1,9 @@
+//
+//  RandomMap.cpp
+//  TesMap
+//
+//  Created by Wansheng Li on 16/10/10.
+//
+//
+
+#include "RandomMap.hpp"
---
+++ Classes/RandomMap.hpp
@@ -0,0 +1,14 @@
+//
+//  RandomMap.hpp
+//  TesMap
+//
+//  Created by Wansheng Li on 16/10/10.
+//
+//
+
+#ifndef RandomMap_hpp
+#define RandomMap_hpp
+
+#include <stdio.h>
+
+#endif /* RandomMap_hpp */
