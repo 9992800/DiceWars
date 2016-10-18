fe85420c20e8c4b23ea06aeca15a1fbdace0a9d2
wsli <99927800@qq.com>
2016-10-18 19:57:45 +0800

    screen size

---
+++ Classes/RandomMap/MapBasicInfo.cpp
@@ -0,0 +1,31 @@
+//
+//  MapBasicInfo.cpp
+//  DiceWars
+//
+//  Created by wsli on 16/10/18.
+//
+//
+#include "MapBasicInfo.hpp"
+
+static MapResolustionConfig _curConfig;
+
+void initConfig(int solution){
+        switch (solution) {
+                case MapResolustion::BIG_SCREEN_RESOLUTION:
+                        _curConfig = {60, 70, 34, 60, 70, "test5.png", 180, 140};
+                        break;
+                case MapResolustion::MEDIUM_SCREEN_RESOLUTION:
+                        _curConfig = {30, 35, 17, 30, 35, "test4.png", 90, 70};
+                        break;
+                        
+                default:
+                case MapResolustion::SMALL_SCREEN_RESOLUTION:
+                        _curConfig = {15, 17, 8, 15, 17, "test6.png", 45, 35};
+               
+                        break;
+        }
+}
+
+const MapResolustionConfig& getConfig(){
+        return  _curConfig;
+}
